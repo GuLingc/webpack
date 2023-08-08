a1a58052639a28fc0563995b8b04132c9e5d10b5
yexi <2794957189@qq.com>
2023-08-08 16:45:20 +0800

    实现对ts语法的实时监听

--- 03-babel/src/ts/math.ts
+++ 03-babel/src/ts/math.ts
@@ -1,6 +1,7 @@
 export function sum(num1: number, num2: number) {
   return num1 + num2;
 }
+
 export function formatPrice(priceString: string) {
     //因为下方的ts中使用了es新推出的includes()方法，为了进行正常的使用所以会对其进行补丁和预设
   if (priceString.includes('$')) {
