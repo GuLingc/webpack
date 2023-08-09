909bcf4d86c5a09480ebb168e0424448aa17a907
yexi <2794957189@qq.com>
2023-08-09 15:26:37 +0800

    静态资源的配置

--- 04-webpack服务器/webpack.config.js
+++ 04-webpack服务器/webpack.config.js
@@ -15,7 +15,11 @@ module.exports = {
   },
   resolve: {
     //可以是引入文件的时候不加文件的后缀名
-    extensions: [".js", ".json", ".wasm", ".jsx", ".vue",'.ts'],
+    extensions: [".js", ".json", ".wasm", ".jsx", ".vue", ".ts"],
+  },
+  //存放静态文件的文件夹，如果文件夹名称是public则可以默认不写，因为它是默认值
+  devServer: {
+    static: ["public"],
   },
   module: {
     rules: [
@@ -36,11 +40,11 @@ module.exports = {
       },
       //解析ts
       {
-      test: /\.ts$/,
-      // use: "ts-loader" //安装ts-loader：npm install ts-loader -D   下一步创建tsconfig文件：tsc --init
-      //为了方便以后的运行配置，或者如果ts文件中出现了es新出的方法进行补丁建议使用babel-loader
-      use: 'babel-loader'
-      }
+        test: /\.ts$/,
+        // use: "ts-loader" //安装ts-loader：npm install ts-loader -D   下一步创建tsconfig文件：tsc --init
+        //为了方便以后的运行配置，或者如果ts文件中出现了es新出的方法进行补丁建议使用babel-loader
+        use: "babel-loader",
+      },
     ],
   },
   //打包html文件
